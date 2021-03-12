233602cae436b44f24314397f0cc754117368c2b
dwwr <dwwarnerreyes@gmail.com>
2021-03-12 14:36:38 -0800

    experimenting with zdog

--- src/app/components/Bubble.js
+++ src/app/components/Bubble.js
@@ -3,14 +3,16 @@ import {StyledBubble} from './styles/BubbleStyles';
 import {StyledCube} from './styles/CubeStyles';
 import {StyledPyramid} from './styles/PyramidStyles';
 import {StyledSphere} from './styles/SphereStyles';
-import { Illustration, Ellipse, Shape, Group, useRender  } from 'react-zdog';
+import { Anchor, Illustration, Ellipse, Shape, Group, useRender  } from 'react-zdog';
+import { a, useSpring } from 'react-spring/zdog';
 
-
+const TAU = Math.PI * 2
 const Test = () => {
   return (
     <Shape
     path={[{ x: 0, y: -8 }, { x: 8, y: 8 }, { x: -8, y: 8 }]}
     translate={{ z: 10 }}
+    rotate={{ z: -TAU / 4 }}
     color="#E62"
     stroke={3}
     fill
@@ -18,13 +20,16 @@ const Test = () => {
   );
 }
 const Bubble = (props) => {
+  const ref = useRef()
+  let t = 0
+  useRender(() => (ref.current.rotate.y = Math.cos((t += 0.1) / TAU)))
 
   return (
     <StyledBubble>
       {/* <StyledSphere />
       <StyledPyramid />
       <StyledCube /> */}
-      <Illustration zoom={10} height={100} style = {{gridColumnStart: '3'}}>
+      <Illustration zoom={10} height={100} ref={ref} style = {{gridColumnStart: '3'}}>
       <Test />
   </Illustration>
     </StyledBubble >
