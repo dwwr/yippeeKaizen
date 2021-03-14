3cbc53e4ae8fded328073bce4b8efa474fe1d80c
dwwr <dwwarnerreyes@gmail.com>
2021-03-14 13:17:20 -0700

    basic animation working with react-zdog

--- src/app/components/Bubble.js
+++ src/app/components/Bubble.js
@@ -3,12 +3,22 @@ import {StyledBubble} from './styles/BubbleStyles';
 import {StyledCube} from './styles/CubeStyles';
 import {StyledPyramid} from './styles/PyramidStyles';
 import {StyledSphere} from './styles/SphereStyles';
-import { Anchor, Illustration, Ellipse, Shape, Group, useRender  } from 'react-zdog';
+import { Anchor, Illustration, Ellipse, Shape, Group, useRender, useZdog } from 'react-zdog';
 import { a, useSpring } from 'react-spring/zdog';
 
 const TAU = Math.PI * 2
 const Test = () => {
+  const [visible, setVisible] = useState(true)
+  useEffect(() => {
+    // Test taking an object away
+    setTimeout(() => setVisible(false), 2000)
+  }, [])
+
+  const group = useRef()
+  useRender(() => (group.current.rotate.y += 0.1))
+
   return (
+    <Group ref={group}>
     <Shape
     path={[{ x: 0, y: -8 }, { x: 8, y: 8 }, { x: -8, y: 8 }]}
     translate={{ z: 10 }}
@@ -17,19 +27,21 @@ const Test = () => {
     stroke={3}
     fill
   />
+  </Group>
   );
 }
 const Bubble = (props) => {
-  const ref = useRef()
-  let t = 0
-  useRender(() => (ref.current.rotate.y = Math.cos((t += 0.1) / TAU)))
+
+
+
 
   return (
     <StyledBubble>
       {/* <StyledSphere />
       <StyledPyramid />
       <StyledCube /> */}
-      <Illustration zoom={10} height={100} ref={ref} style = {{gridColumnStart: '3'}}>
+
+      <Illustration zoom={10} height={100}  style = {{gridColumnStart: '3'}}>
       <Test />
   </Illustration>
     </StyledBubble >
